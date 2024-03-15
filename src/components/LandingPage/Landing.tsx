028b854035f449dac7de5af0c3b7e02f97ac5c84
Oti Desmond <desmondotid.don16@gmail.com>
2024-03-15 12:38:30 +0100

    nav bar in landing page

--- src/components/LandingPage/Landing.tsx
+++ src/components/LandingPage/Landing.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './Landing.css'; // Make sure you have this CSS file
 import { Link } from 'react-router-dom';
 import logo from './logo3.svg';
-
+import { RiSearch2Line } from "react-icons/ri";
+import { LuHeart } from "react-icons/lu";
+import { PiShoppingCartBold } from "react-icons/pi";
+import { FaRegUser } from "react-icons/fa";
 const Landing = () => {
     return (
         <nav className="navbar">
@@ -10,11 +13,26 @@ const Landing = () => {
                 <img src={logo} alt="ShoePlug Logo" />
             </div>
             <div className="search-bar"> 
+            <div className="search-logo-container">
+            <RiSearch2Line className='search-logo' size="25px" />
+            </div>
+            <div className='likes-container'>
+            <LuHeart className='likes' size= "20px"/>
+            </div>
+            <div className='cart-container'>
+            <PiShoppingCartBold className='cart' size="20px"/>
+            </div>
+            <div className='user-container'>
+            <FaRegUser className='user'  size="20px"/>
+            </div>
                 <input type="text" placeholder="Search for shoes" /> 
             </div> 
-            <Link to="/">Home</Link> 
-            <Link to="/profile">Profile</Link>
-            <Link to="/settings">Settings</Link>
+            <div>
+            <Link to="/Home">HOME</Link> 
+            <Link to="/Trending">TRENDING</Link>
+            <Link to="/NewArrivals">NEW ARRIVALS</Link>
+            <Link to="/Collections">COLLECTIONS</Link>
+            </div>
         </nav>
     );
 };
