14c29c2018ce205e18c29af2e4d7f72fb39b183d
Oti Desmond <desmondotid.don16@gmail.com>
2024-03-22 14:47:19 +0100

    fixed issues

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const vite_1 = require("vite");
-const plugin_react_swc_1 = __importDefault(require("@vitejs/plugin-react-swc"));
+
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react-swc'; 
+
 // https://vitejs.dev/config/
-exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_swc_1.default)()],
-});
+export default defineConfig({
+    plugins: [react()],
+
+    // ... Your other Vite configurations, if any 
+    build: {
+        rollupOptions: {
+          input: {
+            // Your existing inputs 
+          },
+          output: {
+            // Your existing outputs
+          },
+          loader: {
+            '.js': 'jsx' 
+          }
+        }
+      }
+    });
+    
